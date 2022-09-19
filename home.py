dce66a733b0f44d60538f1df75afd877e461f162
StefanBloemheuvel <stefanbloemheuvel@gmail.com>
2022-09-19 14:33:34 +0200

    added spaces

--- home.py
+++ home.py
@@ -33,6 +33,7 @@ def return_homepage():
 
         st.write("""
                 Disclaimer of Warranty.
+                
                 There is no warranty for the program, to the extent permitted by applicable law. 
                 except when otherwise stated in writing the copyright holders and/or other parties provide the program “as is” without warranty of any kind, either expressed or implied, 
                 including, but not limited to, the implied warranties of merchantability and fitness for a particular purpose. 
@@ -40,6 +41,7 @@ def return_homepage():
                 should the program prove defective, you assume the cost of all necessary servicing, repair or correction.
 
                 Limitation of Liability.
+                
                 In no event unless required by applicable law or agreed to in writing will any copyright holder, or any other party who modifies and/or conveys the program as permitted above, 
                 be liable to you for damages, including any general, special, 
                 incidental or consequential damages arising out of the use or inability to use the program (including but not limited to loss of data or data being rendered inaccurate or losses sustained by you or third parties or a failure of the program to operate with any other programs), 
