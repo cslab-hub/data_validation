125625ec855d391baf87a281bb282f246da467dc
JvdHoogen <57524082+JvdHoogen@users.noreply.github.com>
2022-11-29 16:41:35 +0100

    check for first 4 chapters
    improved readability

--- fileformat.py
+++ fileformat.py
@@ -8,13 +8,13 @@ def return_file_format():
     st.title('File Formats')
 
     st.markdown("""
-    Storage systems store your data as files and these different formats provide benefits to the users.
+    Storage systems store your data in files with different formats. These different formats provide benefits to the users.
     File formats are specificaly designed to store specific types of information.
-    How you store your data is critical and you need to consider the most optimal format to work with.
+    How you store your data is critical so you need to consider the most optimal format to work with.
     Things to consider are:
 
     - Structure of your data
-    - Easy to read
+    - Easy to read and understand
     - Compatability
 
     """)
@@ -25,7 +25,7 @@ def return_file_format():
                 ### Take a look at your dataset in the folder where it is stored.\n
                 The file contains a specific file extension which is found after the filename.
                 For example, a file called dataset.csv has the file format comma-separated-value (.csv).
-                Excel files would have the name: .xlsx or .xsl.
+                Excel files would have the name: .xlsx or .xsl and files from text editors end with .txt.
                 
                 After you have found your file format, you can select it in the drop-down menu below:
                 """)
@@ -68,7 +68,7 @@ def return_file_format():
                         , unsafe_allow_html=True)
 
         st.markdown('')
-        st.warning('Also try saving your data with a comma that separates values: Var1, Var2 instead of Var1; Var2. That makes it easier to process the data in software since they assume a comma as the seperator between values.')
+        st.warning('Also try saving your data with a comma that separates values: Var1, Var2 instead of Var1; Var2. That makes it easier to process the data in software since it assumes a comma as the seperator between values.')
         
     
     if option == 'excel':
@@ -83,11 +83,11 @@ def return_file_format():
 
         st.markdown("""
         #### For simple manipulation of your data, and if programming knowledge is not available in your organization, we recommend to stick to Excel.
-        However, if you are interested to learn more about Data Science, the standard way to store data is with a Comma Seperated Value file, called .csv.
-        These files are just plain text files that store your data, which can be very easily used in any programming languages to manipulate them.
+        However, if you are interested to learn more about data analytics, the standard way to store data is with a Comma Seperated Value file, called .csv.
+        These files are just plain text files that store your data, which can be very easily used in any programming language to manipulate them.
         
-        For example, Table 2 shows a very simple dataset just like is visible in Figure 1. 
-        However, the data format is just a plain text file which can be copy-pasted bellow and used in other programs or software:
+        For example, Table 2 shows a very simple dataset similar to Figure 1. 
+        However, the data format is just a plain text file which can be copy-pasted below and used in other programs or software:
         """)
         
         st.write(pd.DataFrame({
@@ -115,6 +115,7 @@ def return_file_format():
                         , unsafe_allow_html=True)
 
         st.markdown("""
+
         This dataset is simply stored in a .csv file that looks like the following:
         """)
 
@@ -131,17 +132,17 @@ def return_file_format():
         
             
     if option == 'txt':
-        st.success('You have an ideal data format!')
-        st.markdown("TXT files are just like .csv files, organised in a [row, column] manner and therefore are easy to work with.")
+        st.success('You have an ideal data file format!')
+        st.markdown(".txt files are just like .csv files, organised in a [row, column] manner and therefore are easy to work with.")
         
 
     if option == 'parquet':
-        st.warning('Parquet is not the ideal data format')
-        st.markdown('Parquet files are a column-based dataset which can be used, but not many websites support documentation for this data type. Please check if you could ask from your data supplier if the data could be supplied in .csv or .txt format')
+        st.warning('Parquet is not the ideal data file format')
+        st.markdown('Parquet files are column-based datasets which can be used. However, not many support documentation can be found on the web that help with processing this type of data. Please check with your data supplier if the data could be converted to .csv or .txt format.')
 
     if option == 'other':
-        st.error('Your dataset format is not commonly used')
-        st.markdown('Please ask your data supplier for a .csv or .txt version of your data, because it is highly likely that errors will occur in the future due to the data format you own.')
+        st.error('Your data file format is not commonly used')
+        st.markdown('Please ask your data supplier for a .csv or .txt version of your data, because it is highly likely that errors will occur in the future due to the data file format you have.')
         
 
     
--- introduction.py
+++ introduction.py
@@ -7,11 +7,11 @@ def return_introduction():
     
     st.title('Why Data Validation?')
     st.markdown("""
-    Welcome to the Data Validation tool, where you will learn alot about all the steps needed before doing any analysis at all.
+    Welcome to the Data Validation tool, where you will learn about all the steps needed before analysing the data.
     One might ask the question why validating your data matters.
-    Well, it turns out that data workflows today are a huge mess.
+    Well, it turns out that data workflows today are quite messy.
     
-    To start, (1) data pipelines are fragile and fail often, which can cause problems later on in the analysis steps.
+    To start, (1) data pipelines are fragile and fail often, which can cause problems later on in the analysis.
     Look back at all the steps needed to get the data from your machines or IT systems in the first place.
     All that effort only included retrieving the data in its raw form, now we also need to validate it.
 
@@ -22,19 +22,19 @@ def return_introduction():
 
     st.title('Goal of the tool')
     st.markdown("""
-    It turns out that every company has widely different data formats retrieved from their machines/systems.
+    It turns out that every company has different data formats retrieved from their machines/systems.
     The goal of the Data Validation tool is to help you prepare your data for the Data Analytics tool.
-    Therefore, at the end, you will hopefully be able to adapt your data in order to make it suitable for later on analysis.
+    Therefore, at the end, you will hopefully be able to adapt your data in order to make it suitable for analysis.
     Chapter 9 shows how the data should look like after all the preprocessing steps explained in the preceding chapters.
     """)
     st.title('Tip: Find a Text Editor.')
     st.markdown("""
             Before you start reading the chapters, we advice to install a text editor to examine your data.
-             After you have collected your data in either .csv or .txt, we have to inspect our data.
-             Most likely you are used to Excel, however, for data analysis purposes, there are better options!
+             After you have collected your data in either .csv or .txt, you have to inspect the data.
+             Most likely you are used to work with Excel, however, for data analysis purposes, there are better options!
              
              We recommend [Sublime Text](https://www.sublimetext.com/), which can open almost any file format.
-             Another great advantage of Sublime Text is that it open files significantly faster, and it is open source.
+             Another great advantage of Sublime Text is that it open files significantly faster, and it is open source meaning it can be used for free.
              """)
     image = Image.open('images/sublime.png')
     st.image(image, caption='Figure 1: Example of Sublime Text', use_column_width=True)
